a6ad03fb057a5b26ff07cac3732e274ab28762de
Kvivriz <163144666+Kvivriz@users.noreply.github.com>
2024-10-25 16:38:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.querySelector('.mudarTextoBotao').addEventListener('click', function() {
 
-const paragrafo document.querySelector('.descricao'); paragrafo.textContent = 'Texto alterado!';
+const paragrafo = document.querySelector('.descricao'); 
+paragrafo.textContent = 'Texto alterado!';
 
 });
 
 document.querySelector('.destacarBotao').addEventListener('click', function() {
 
-const titulo document.querySelector('.titulo');
+const titulo = document.querySelector('.titulo');
 
 titulo.classList.add('destaque');
 
